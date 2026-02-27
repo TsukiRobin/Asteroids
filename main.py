211a6afa5887481779662751a00d60ec1bce6636
Robin Rogner <robinrogner@live.se>
2026-02-27 13:53:14 +0100

    Updated and Created Player Class

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+from player import Player
+from circleshape import CircleShape
 from logger import  log_state
 from constants import SCREEN_HEIGHT,SCREEN_WIDTH
 
@@ -8,13 +10,24 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+
 
     while(True):
         log_state()
         for event in pygame.event.get():
-            pass
+            if event.type == pygame.QUIT:
+                return
+
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
+        
 
 if __name__ == "__main__":
     main()
